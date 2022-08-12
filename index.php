4692eaab2eff8cf01c4d480b10a2d6130af1dfa2
higways88 <111070851+higways88@users.noreply.github.com>
2022-08-13 03:10:55 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html style="color-scheme: light; --liff-base-background-color: #FFFFFF; --liff-base-text-color: #000000; --liff-base-background-rgb-color: 255, 255, 255; --liff-base-text-rgb-color: 0, 0, 0; --liff-light-button-border-color: rgba(0, 0, 0, 0.15); --liff-title-text-color: #111111; --liff-title-background-color: #FFFFFF; --liff-title-button-color: #111111; --liff-icon-color: #111111; --liff-status-bar-color: black; --liff-title-subtext-color: #B7B7B7; --liff-progress-bar-color: #06C755; --liff-progress-background-color: #FFFFFF; --liff-title-button-area-background-color: rgba(255, 255, 255, 0.12); --liff-title-button-area-border-color: rgba(0, 0, 0, 0.15);" lang="en"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>HIGHWAYS88 คาสิโนออนไลน์ เสือมังกร เกมส์ยิงปลา สล็อต อันดับหนึ่งของไทย</title>
+<meta property="og:locale" content="th_TH">
+<meta property="og:type" content="game mobile">
+<meta property="og:image" content="/wowslot1688/logo.jpg">
+<meta property="og:url" content="wowslot1688.com">
+<meta property="og:type" content="games">
+<meta property="og:title" content="ชวนเพื่อนมาเล่นกับเรา รับเงินตลอดชีพ / รัยค่าคอมตลอดชีพ/ รับเครดิตฟรี 10 %">
+<meta property="og:description" content="ชวนเพื่อนมาเล่นกับเรา รับเงินตลอดชีพ / รัยค่าคอมตลอดชีพ/ รับเครดิตฟรี 10 %">
+<link rel="stylesheet" href="WOWSLOT1688%20%E0%B9%80%E0%B8%81%E0%B8%A1%E0%B8%AA%E0%B8%A5%E0%B9%87%E0%B8%AD%E0%B8%95%E0%B8%AD%E0%B8%AD%E0%B8%99%E0%B9%84%E0%B8%A5%E0%B8%99%E0%B9%8C_files/bootstrap.min.css">
+<link rel="stylesheet" href="WOWSLOT1688%20%E0%B9%80%E0%B8%81%E0%B8%A1%E0%B8%AA%E0%B8%A5%E0%B9%87%E0%B8%AD%E0%B8%95%E0%B8%AD%E0%B8%AD%E0%B8%99%E0%B9%84%E0%B8%A5%E0%B8%99%E0%B9%8C_files/style.css">
+<style>
+        body {
+            background:url('https://www.img.in.th/images/0875fd6dd75ad2804d53218b44c604d1.jpg');
+        }
+    </style>
+</head>
+<body>
+<div class="cover-container d-flex align-items-center">
+<div class="text-center mx-auto">
+
+<img src="https://www.img.in.th/images/d7938608bc71ce3704b9792f7939c802.png" class="img-fluid" alt="...">
+<div class="text-danger font-weight-bold mt-5">
+เพียงแชร์ต่อไปให้เพื่อน แล้วส่งข้อมูลให้แอดมิน ก็ได้รับเครดิตฟรีไปเลย  50 บาททันที!!
+</div>
+<button class="btn btn-primary w-50 share-btn mt-3" size="sm" onclick="sendShare()">แชร์</button>
+</div>
+</div>
+<script charset="utf-8" src="WOWSLOT1688%20%E0%B9%80%E0%B8%81%E0%B8%A1%E0%B8%AA%E0%B8%A5%E0%B9%87%E0%B8%AD%E0%B8%95%E0%B8%AD%E0%B8%AD%E0%B8%99%E0%B9%84%E0%B8%A5%E0%B8%99%E0%B9%8C_files/bootstrap.bundle.min.js"></script>
+<script charset="utf-8" src="WOWSLOT1688%20%E0%B9%80%E0%B8%81%E0%B8%A1%E0%B8%AA%E0%B8%A5%E0%B9%87%E0%B8%AD%E0%B8%95%E0%B8%AD%E0%B8%AD%E0%B8%99%E0%B9%84%E0%B8%A5%E0%B8%99%E0%B9%8C_files/sdk.js"></script>
+<script>
+      const siteName = 'Highways88'
+      const lineUrl = 'https://highways88.com/'
+      const registerUrl = 'https://sport.playauto.cloud/register?prefix=SFdT&recommend=4'
+      const liffId = '1657380590-X0dnV1xE'
+
+      async function sendShare() {
+        const result = await liff.shareTargetPicker([
+          {
+            "type": "flex",
+            "altText": `${siteName.toUpperCase()} แชร์ต่อรับเครดิตฟรี`,
+            "contents": {
+              "type": "bubble",
+              "hero": {
+                "type": "image",
+                "url": `https://sv1.picz.in.th/images/2022/08/12/XntHeI.png`,
+                "size": "full",
+                "aspectRatio": "1.91:1",
+                "action": {
+                  "type": "uri",
+                  "uri": lineUrl
+                }
+              },
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": siteName.toUpperCase(),
+                    "size": "xl",
+                    "weight": "bold",
+                    "align": "center"
+                  },
+                  {
+                    "type": "text",
+            "text": "แจกฟรี 50 บาท ",
+            "gravity": "bottom",
+						"color": "#c59292",
+            "align": "center"
+
+                  },
+					{
+            "type": "text",
+            "text": "รวมสล็อตออนไลน์ไว้ในที่เดียว สมัครฟรี",
+				"color": "#c59292",
+            "align": "center"
+          }
+
+
+                ]
+              },
+              "footer": {
+                "type": "box",
+                "layout": "horizontal",
+                "spacing": "md",
+                "contents": [
+                  {
+                    "type": "button",
+                    "style": "primary",
+                    "action": {
+                      "type": "uri",
+                      "label": "สมัครสมาชิก",
+                      "uri": `https://sport.playauto.cloud/register?prefix=SFdT&recommend=10/?openExternalBrowser=1`
+                    }
+                  },
+                  {
+                    "type": "button",
+                    "style": "primary",
+                    "action": {
+                      "type": "uri",
+                      "label": "รับเครดิตฟรี",
+                      //"uri": "https://hw88.highwayruay.online/?openExternalBrowser=1"
+					  "uri": `https://lin.ee/Dkkzgej`
+
+
+                    }
+                  }
+                ]
+              }
+            }
+          }
+        ])
+
+        if (result) {
+          liff.closeWindow()
+        }
+      }
+
+      async function main() {
+        await liff.init({ liffId })
+        if (!liff.isLoggedIn()) {
+          liff.login()
+        }
+      }
+      main()
+    </script><script src="WOWSLOT1688%20%E0%B9%80%E0%B8%81%E0%B8%A1%E0%B8%AA%E0%B8%A5%E0%B9%87%E0%B8%AD%E0%B8%95%E0%B8%AD%E0%B8%AD%E0%B8%99%E0%B9%84%E0%B8%A5%E0%B8%99%E0%B9%8C_files/non-ios-extensions.js" type="text/javascript"></script>
+<script defer="defer" src="WOWSLOT1688%20%E0%B9%80%E0%B8%81%E0%B8%A1%E0%B8%AA%E0%B8%A5%E0%B9%87%E0%B8%AD%E0%B8%95%E0%B8%AD%E0%B8%AD%E0%B8%99%E0%B9%84%E0%B8%A5%E0%B8%99%E0%B9%8C_files/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon="{&quot;rayId&quot;:&quot;73789cfe9cdc26c1&quot;,&quot;version&quot;:&quot;2022.6.0&quot;,&quot;r&quot;:1,&quot;token&quot;:&quot;3ca21dc6819a47b988c0cbca0264eca1&quot;,&quot;si&quot;:100}" crossorigin="anonymous"></script>
+
+</body></html>
